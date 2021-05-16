3dab10ade71c1e02294cb470529b6e9544e9feb5
mikee47 <mike@sillyhouse.net>
2021-05-16 15:51:31 +0100

    Update file headers

--- src/include/HSPI/Request.h
+++ src/include/HSPI/Request.h
@@ -1,17 +1,26 @@
-/****
- * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
- * Created 2015 by Skurydin Alexey
- * http://github.com/anakod/Sming
- * All files of the Sming Core are provided under the LGPL v3 license.
- *
+/**
  * Request.h
+ *
+ * Copyright 2018 mikee47 <mike@sillyhouse.net>
+ *
+ * This file is part of the HardwareSPI Library
+ *
+ * This library is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, version 3 or later.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this library.
+ * If not, see <https://www.gnu.org/licenses/>.
  *
  * @author: 11 December 2018 - mikee47 <mike@sillyhouse.net>
  *
  * Definitions for an SPI Request Packet containing data and settings for a transfer.
  * A single transfer may use 1 or more transactions.
  *
- */
+ ****/
 
 #pragma once
 
